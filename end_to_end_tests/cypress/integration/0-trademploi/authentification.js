1894f4e2b794c743d547eadccf86c7281b058cbb
akourlaiev <artem.kourlaiev@soprasterianext.com>
2021-08-10 16:40:18 +0200

    TRAD-238 init with cypress default and TradEmploi end to end test

---
+++ end_to_end_tests/cypress/integration/0-trademploi/authentification.js
@@ -0,0 +1,81 @@
+// npx cypress open
+
+const URL = 'https://pole-emploi-trad-dev.firebaseapp.com'
+const LOGIN = 'agent@pe.fr'
+const PWD = 'm2g@0i10x6CP'
+
+describe('Auth component', () => {
+    beforeEach(() => {
+
+    })
+
+    it('user connection OK', () => {
+        cy.visit(URL)
+        cy.url().should('include', '/auth')
+        cy.get('#email').type(LOGIN)
+        cy.get('#password').type(PWD)
+        cy.contains('SE CONNECTER').click()
+        cy.contains('Authentification réussie').should("exist")
+        cy.url().should('include', '/modality')
+        cy.get('#logout-header').click()
+        cy.get('.logout-btn').click()
+        cy.url().should('include', '/start');
+        cy.url().should('include', '/auth')
+
+    })
+
+    it('user try to connect with nothing values', () => {
+        cy.url().should('include', '/auth')
+        cy.contains('SE CONNECTER').should('be.disabled')
+        cy.url().should('include', '/auth')
+    })
+
+    it('messages d alerte lors de la saisie', () => {
+        cy.url().should('include', '/auth')
+
+        cy.get('#email').click();
+        cy.get('#password').click();
+        cy.get('#mat-error-5').should('have.text', 'L\'adresse email est requise.');
+
+        cy.get('#email').click();
+        cy.get('#mat-error-6').should('have.text', 'Le mot de passe est requis.');
+
+        cy.get('#email').clear();
+        cy.get('#email').type('test');
+        cy.get('#mat-error-7').should('have.text', 'Vous devez entrer une adresse email valide.');
+        cy.get('#email').clear();
+        cy.get('#email').type('agent@pe.fr');
+
+        cy.get('#password').click();
+        cy.get('#password').type('test');
+
+        cy.get('#mat-error-10').should('have.text', 'Le mot de passe doit faire au moins 6 caractères.');
+        cy.get('#password').clear();
+        cy.get('#password').type('test123456');
+
+    })
+
+    it('user connection KO : mauvais login', () => {
+        cy.url().should('include', '/auth')
+        cy.get('#email').clear();
+        cy.get('#email').type('login_error@pe.fr')
+
+        cy.get('#password').clear();
+        cy.get('#password').type(PWD)
+        cy.contains('SE CONNECTER').click()
+        cy.url().should('include', '/auth')
+        cy.contains('La base de donnée est indisponible momentanément. Merci de réessayer plus tard').should("exist")
+    })
+
+    it('user connection KO : mauvais mot de passe', () => {
+        cy.url().should('include', '/auth')
+        cy.get('#email').clear();
+        cy.get('#email').type(LOGIN)
+
+        cy.get('#password').clear();
+        cy.get('#password').type('password error')
+        cy.contains('SE CONNECTER').click()
+        cy.url().should('include', '/auth')
+        cy.contains('Le mot de passe est invalide, veuillez vérifier l\'adresse web et le mot de passe').should("exist")
+    })
+})
---
+++ end_to_end_tests/cypress/integration/0-trademploi/mono_support_full_test.js
@@ -0,0 +1,93 @@
+// npx cypress open
+const URL = 'https://pole-emploi-trad-dev.firebaseapp.com'
+const LOGIN = 'agent@pe.fr'
+const PWD = 'm2g@0i10x6CP'
+
+describe('Choice component', () => {
+
+  before(() => {
+    cy.clearCookies()
+    cy.visit(URL);
+    cy.url().should('include', '/auth');
+    cy.get('#email').type(LOGIN);
+    cy.get('#password').type(PWD);
+    cy.contains('SE CONNECTER').click();
+
+    cy.url().should('include', '/modality');
+    cy.contains('Authentification réussie').should('exist')
+
+    cy.contains('Mono support').click();
+    cy.contains('CONFIRMER').click();
+    cy.url().should('include', '/choice');
+  })
+
+  it('jouer avec le menu', () => {
+    cy.get(':nth-child(1) > .option > :nth-child(3) > .speaker').click();
+
+    cy.get('.no-clicked > img').click();
+    cy.get('.fa').click();
+    cy.get('.no-clicked').click();
+    cy.get(':nth-child(2) > .option > :nth-child(3) > .speaker').click();
+    cy.get('#gdpr-header > .name').click();
+    cy.get('.mat-select-arrow-wrapper').click();
+    cy.get('#mat-option-0 > .mat-option-text').click();
+    cy.get('.options').click();
+    cy.get('#help-header > .name').click();
+    cy.get('[aria-label="Suivant"]').click();
+    cy.get('[aria-label="Suivant"]').click();
+    cy.get('[aria-label="Suivant"]').click();
+    cy.get('[aria-label="Suivant"]').click();
+    cy.get('[aria-label="Précédent"]').click();
+    cy.get('[aria-label="Précédent"]').click();
+    cy.get('[aria-label="Précédent"]').click();
+    cy.get('[aria-label="Précédent"]').click();
+    cy.get('[aria-label="Précédent"]').click();
+    cy.get('.slide-top > .mat-icon').click();
+    cy.get('.fas').click();
+
+    cy.get(':nth-child(1) > .flag > .flag-icon').click();
+    cy.get('#choice-header > .name').click();
+    cy.get('.fa').click();
+
+    // choix langue anglaise
+    cy.get(':nth-child(2) > .title > .fr').click();
+    cy.url().should('include', '/translation');
+
+  });
+
+  it('faire un court échange en anglais', () => {
+    cy.url().should('include', '/translation');
+
+    // saisi bonjour par le conseiller
+    cy.get('#msg-wrapper-advisor').type('bonjour')
+    cy.get('#advisor-wrapper > .container > .actions > .ng-star-inserted').click();
+
+    cy.get('.messageRaw').should('have.text', ' bonjour ');
+    cy.get('bdi').should('have.text', 'Hello');
+
+    // saisi good morning par le DE
+    cy.get('#msg-wrapper-guest').type('good morning')
+    cy.get('[role="DE"] > .container > .actions > .ng-star-inserted').click();
+
+    cy.get('.user > .messageRaw').should('have.text', ' good morning ');
+    cy.get('.user > .messageTranslated > bdi').should('have.text', 'Bonjour');
+
+    // clic sur terminer la conversation
+    cy.get('.end-btn').click();
+
+    // évaluation étoile
+    cy.get(':nth-child(1) > .stars > :nth-child(4) > .mat-icon').click();
+    cy.get(':nth-child(2) > .stars > :nth-child(4) > .mat-icon').click();
+
+    cy.get('#question-four-yes').click();
+    cy.get('#question-four-no').click();
+
+    cy.get('.question > textarea').type('test cypress');
+
+    // clic sur soumettre
+    cy.get('#send-btn').click();
+    //cy.contains('Le formulaire d\'évaluation n\'a pas pu être envoyée, il n\'est pas possible de le resaisir. Vous allez être redirigé vers la page d\'accueil.').should('exist')
+    cy.url().should('include', '/auth');
+  });
+
+});
