5828a3b3e53af160f34468578362ca63f3609a47
akourlaiev <artem.kourlaiev@soprasterianext.com>
2021-08-30 18:04:43 +0200

    TRAD-254 cypress : update authetification test : connexion / and deconexion only. Don't test fail login / pwd

--- end_to_end_tests/cypress/integration/trademploi/authentification.js
+++ end_to_end_tests/cypress/integration/trademploi/authentification.js
@@ -1,5 +1,6 @@
 // npx cypress open
-const URL = 'https://pole-emploi-trad-dev.firebaseapp.com/start'
+const URL = 'https://pole-emploi-trad-dev.firebaseapp.com'
+
 const LOGIN = 'test@pe.fr'
 const PWD = 'trademploi2021'
 
@@ -8,69 +9,24 @@ describe('Auth component', () => {
         cy.clearCookies()
         cy.visit(URL)
         cy.url().should('include', '/auth')
-        cy.get('#email').type(LOGIN)
-        cy.get('#password').type(PWD)
-        cy.contains('SE CONNECTER').click()
-        cy.contains('Authentification réussie').should("exist")
+        cy.get('.t2').should('have.text', 'Connexion');
+        cy.get('.t3').should('have.text', 'Accédez au service de traduction instantanée');
+        cy.get('button').should('have.text', 'Se connecter avec Pôle Emploi');
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('button').click();
         cy.url().should('include', '/modality')
-        cy.get('#logout-header').click()
-        cy.get('.logout-btn').click()
-        cy.url().should('include', '/start');
+        cy.get('app-modality.ng-star-inserted').click();
+        cy.get('.confirmBtn').should('have.text', 'CONFIRMER');
+        cy.get('.t2').should('have.text', 'Modalité de conversation');
+        cy.get('#logout-header > .name').click();
+        cy.get('#mat-dialog-title-0').should('have.text', 'Déconnexion');
+        cy.get('.question').click();
+        cy.get('.question').should('have.text', 'Voulez-vous vous déconnecter?');
+        cy.get('.logout-btn').should('have.text', 'SE DÉCONNECTER');
+        cy.get('.logout-btn').click();
         cy.url().should('include', '/auth')
-
-    })
-
-    it('user try to connect with nothing values', () => {
-        cy.url().should('include', '/auth')
-        cy.contains('SE CONNECTER').should('be.disabled')
-        cy.url().should('include', '/auth')
-    })
-
-    it('messages d alerte lors de la saisie', () => {
-        cy.url().should('include', '/auth')
-
-        cy.get('#email').click();
-        cy.get('#password').click();
-        cy.get('#mat-error-5').should('have.text', 'L\'adresse email est requise.');
-
-        cy.get('#email').click();
-        cy.get('#mat-error-6').should('have.text', 'Le mot de passe est requis.');
-
-        cy.get('#email').clear();
-        cy.get('#email').type('test');
-        cy.get('#mat-error-7').should('have.text', 'Vous devez entrer une adresse email valide.');
-        cy.get('#email').clear();
-        cy.get('#email').type('agent@pe.fr');
-
-        cy.get('#password').click();
-        cy.get('#password').type('test');
-
-        cy.get('#mat-error-10').should('have.text', 'Le mot de passe doit faire au moins 6 caractères.');
-        cy.get('#password').clear();
-        cy.get('#password').type('test123456');
-    })
-
-    it('user connection KO : mauvais login', () => {
-        cy.url().should('include', '/auth')
-        cy.get('#email').clear();
-        cy.get('#email').type('login_error@pe.fr')
-
-        cy.get('#password').clear();
-        cy.get('#password').type(PWD)
-        cy.contains('SE CONNECTER').click()
-        cy.url().should('include', '/auth')
-        cy.contains('Le login ou le mot de passe sont invalides').should("exist")
-    })
-
-    it('user connection KO : mauvais mot de passe', () => {
-        cy.url().should('include', '/auth')
-        cy.get('#email').clear();
-        cy.get('#email').type(LOGIN)
-
-        cy.get('#password').clear();
-        cy.get('#password').type('password error')
-        cy.contains('SE CONNECTER').click()
-        cy.url().should('include', '/auth')
-        cy.contains('Le login ou le mot de passe sont invalides').should("exist")
+        cy.get('.t2').should('have.text', 'Connexion');
+        cy.get('.t3').should('have.text', 'Accédez au service de traduction instantanée');
+        cy.get('button').should('have.text', 'Se connecter avec Pôle Emploi');
     })
 })
